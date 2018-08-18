486ab5f67ced81e7ac02ca678bac315ef12d55d6
Avishek singh <avisingh3131@gmail.com>
2018-08-18 23:52:31 +0530

    init commit

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const path=require('path');
 const express=require('express');
 
 const publicPath=path.join(__dirname+'/../public');
-const port=process.env.Port||3000;
+const Port=process.env.Port||3000;
 var app=express();
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+app.listen(Port,()=>{
     console.log('server is up on port 3000');
 });
 
