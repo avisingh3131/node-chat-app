e035fe07e122dd2e3aa7aef78ab88587a1885c7b
Avishek singh <avisingh3131@gmail.com>
2018-08-21 01:11:30 +0530

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -91,7 +91,7 @@ var socket= io();
             
             
             socket.emit('createMessage',{
-                from:'User',
+            
                 text:messageTextbox.val()
             },function(){
              messageTextbox.val('');
